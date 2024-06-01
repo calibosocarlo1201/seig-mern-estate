4a74162a206ce84bac6b6cad608a74ce0f9c1b23
calibosocarlo1201 <calibosocarlo1201@gmail.com>
2024-06-01 20:13:10 +0800

    create middleware and function for error handling

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,26 +1,24 @@
 import User from "../models/user.model.js";
 import bcrypt from 'bcryptjs';
 
-export const signUp = async (req, res) => {
+export const signUp = async (req, res, next) => {
     const { user, email, password } = req.body;
-    console.time("Total Request Time");
-    console.time("Hashing Time");
+    // console.time("Total Request Time");
+    // console.time("Hashing Time");
 
     try {
         const hashedPassword = bcrypt.hashSync(password, 10);
-        console.timeEnd("Hashing Time");
+        //console.timeEnd("Hashing Time");
 
         const newUser = new User({ user, email, password: hashedPassword });
-        console.time("Database Save Time");
+        //console.time("Database Save Time");
         
         await newUser.save();
-        console.timeEnd("Database Save Time");
+        //console.timeEnd("Database Save Time");
 
         res.status(201).json({ message: "User created successfully!" });
     } catch (error) {
-        console.error("Error during sign up:", error);
-        res.status(500).json(error.message);
-    } finally {
-        console.timeEnd("Total Request Time");
+        //console.error("Error during sign up:", error);
+        next(error);
     }
 };
