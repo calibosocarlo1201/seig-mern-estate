c2bece4450c1a3aada8e8912d9d9768dadf3b666
calibosocarlo1201 <calibosocarlo1201@gmail.com>
2024-06-09 14:19:46 +0800

    created user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,38 @@
+import errorHandler from "../utils/error.js"
+import bcrypt from 'bcryptjs'
+import User from '../models/user.model.js'
+
 export const test = (req,res) => {
     res.json({
         message: "test"
     })
+}
+
+export const updateUser = async (req, res, next) => {
+
+    if(req.user.id !== req.params.id) return next(errorHandler(403, 'Unautorized! You can only update your own account'));
+
+    console.log(req.body.user)
+
+    try {
+        if(req.body.password){
+            req.body.password = bcrypt.hashSync(req.body.password, 10);
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: {
+                user: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar
+            }
+        }, {new: true});
+
+        const {password, ...rest} = updatedUser._doc;
+
+        res.status(200).json(rest);
+
+    } catch (error) {
+        next(error);
+    }
 }
